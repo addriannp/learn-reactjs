ab3c9bf7fa5da96062b6a6e4886f7a277424e67c
Adrian Pratama <adriaanpratama@gmail.com>
2019-06-23 02:19:25 -0700

    done learn chapter 1

---
+++ chapter-1/practice-1/script.js
@@ -0,0 +1,64 @@
+// let string1 = "this is a string";
+// let string2 = "another string";
+// let string3 = `${string1} and ${string2}`;
+// console.log('string3\ value is', string3);
+
+// let info = ["first", 100, 200, true, "last"];
+// info[0];
+// info[1];
+// info[4];
+// info.length;
+
+// result = Math.PI;
+// console.log(result);
+
+// result = Math.round(4.7);
+// console.log(result);
+
+// result = Math.round(4.4);
+// console.log(result);
+
+// result = new Date();
+// console.log(result);
+// let year = result.getFullYear();
+
+// let alphabet = ["a", "b", "c", "d"];
+
+// let bigValue = numeral('1,000,000,000,200.00');
+// let integerValue = bigValue.value();
+// console.log(integerValue);
+// console.log('the value is',numeral(integerValue).format('$0,0.00'));
+
+// let secondValue = numeral ('$ 200,500,000');
+// console.log('second\'s value is', secondValue.value());
+
+// moment.locale('id');
+// let today = moment();
+// console.log('today is', today.format('dddd, MMMM Do YYYY'));
+// console.log('next week is ',moment().add(1, 'weeks').format('DD MMMM YYYY'));
+
+// console.log(moment([2019, 4, 5]).fromNow());
+
+// let grade = window.prompt("what's your grade");
+// let myGrade = "";
+
+// if (grade>=90) {
+//     myGrade = "A";
+// } else if (grade>=80){
+//     myGrade = "B";
+// } else if (grade>=70){
+//     myGrade = "C";
+// } else if (grade>=60){
+//     myGrade = "D";
+// } else {
+//     myGrade = "F"
+// }
+
+// window.alert(`my grade is ${myGrade}`);
+
+let textNumber = "";
+debugger;
+for (let counter = 0; counter < 5; counter++) {
+    textNumber += counter + ",";
+}
+console.log(textNumber)
