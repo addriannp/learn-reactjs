36f414269184a09221aeccce9c94cee6da4539a3
wellyandriani <wellyandriani97@gmail.com>
2019-07-06 11:36:45 +0700

    update

--- chapter-2/practice-3/index.js
+++ chapter-2/practice-3/index.js
@@ -19,7 +19,44 @@ const students = [{
 let longGrades = students.filter(student => student.grade < 55);
 console.log('below 55 is ', longGrades)
 
-let maleStudents = ['Budi, Gorge, James'];
 
-const maleStudents1 = maleStudents.map(x = male);
+const maleStudents1 = students.map(student => {
+    student.gender = 'male';
+    return student
+})
 console.log(' Budi Gorge and James are', maleStudents1)
+
+students.forEach( student => {
+    // const grade = student.grade;
+    // const name = student.name;
+    const{ grade, name } = student;
+    console.log(`hi im ${name}, im in ${grade} grade`);
+})
+
+maleStudents1.push(
+    {
+        name: 'Jolie',
+        grade: 99,
+        gender: 'female'
+
+    }
+)
+
+console.group(maleStudents1)
+
+maleStudents1.sort(function(studentA, studentB) {
+    return studentA.name - studentB.name
+})
+
+console.log(maleStudents1);
+
+maleStudents1.sort(function(studentA, studentB){
+    return studentB.name - studentA.name
+})
+
+console.log(maleStudents1);
+
+const studentGradeuate = maleStudents1.filter(student=> {
+    return student.grade >= 55
+})
+console.log(studentGradeuate)
